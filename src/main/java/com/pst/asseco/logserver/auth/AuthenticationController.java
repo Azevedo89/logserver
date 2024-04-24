2c4412678a3a99638ac65d957bb645c549a27372
GONCALO AZEVEDO <goncalosazevedo@gmail.com>
2024-04-24 15:11:26 +0100

    Logout do Back-End configurado

--- src/main/java/com/pst/asseco/logserver/auth/AuthenticationController.java
+++ src/main/java/com/pst/asseco/logserver/auth/AuthenticationController.java
@@ -2,13 +2,21 @@ package com.pst.asseco.logserver.auth;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -29,5 +37,14 @@ public class AuthenticationController {
         @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){    
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout"; 
     }
 }
